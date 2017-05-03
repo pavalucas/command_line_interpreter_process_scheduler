7aa345caa13e5efe989ae32753b44d3e7b48f385
pavalucas <lucasapava@gmail.com>
2017-05-02 22:56:17 -0300

    Added implementation of queue and heap to be used for round robin and priority, respectively. Needs testing

--- scheduler.c
+++ scheduler.c
@@ -1,8 +1,171 @@
 #include "scheduler.h"
 
+Queue *ConstructQueue(int limit) {
+    Queue *queue = (Queue*) malloc(sizeof (Queue));
+    if (queue == NULL) {
+        return NULL;
+    }
+    if (limit <= 0) {
+        limit = 65535;
+    }
+    queue->limit = limit;
+    queue->size = 0;
+    queue->head = NULL;
+    queue->tail = NULL;
+
+    return queue;
+}
+
+void DestructQueue(Queue *queue) {
+    NODE *pN;
+    while (!isEmpty(queue)) {
+        pN = Dequeue(queue);
+        free(pN);
+    }
+    free(queue);
+}
+
+int Enqueue(Queue *pQueue, NODE *item) {
+    /* Bad parameter */
+    if ((pQueue == NULL) || (item == NULL)) {
+        return FALSE;
+    }
+    // if(pQueue->limit != 0)
+    if (pQueue->size >= pQueue->limit) {
+        return FALSE;
+    }
+    /*the queue is empty*/
+    item->prev = NULL;
+    if (pQueue->size == 0) {
+        pQueue->head = item;
+        pQueue->tail = item;
+
+    } else {
+        /*adding item to the end of the queue*/
+        pQueue->tail->prev = item;
+        pQueue->tail = item;
+    }
+    pQueue->size++;
+    return TRUE;
+}
+
+NODE * Dequeue(Queue *pQueue) {
+    /*the queue is empty or bad param*/
+    NODE *item;
+    if (isEmpty(pQueue))
+        return NULL;
+    item = pQueue->head;
+    pQueue->head = (pQueue->head)->prev;
+    pQueue->size--;
+    return item;
+}
+
+int isEmpty(Queue* pQueue) {
+    if (pQueue == NULL) {
+        return FALSE;
+    }
+    if (pQueue->size == 0) {
+        return TRUE;
+    } else {
+        return FALSE;
+    }
+}
+
+void heap_init(struct heap *h)
+{
+    h->count = 0;
+    h->size = 1;
+    h->heaparr = (int *) malloc(sizeof(int));
+    if(!h->heaparr) {
+        printf("Error allocatinga memory...\n");
+        exit(-1);
+    }
+
+}
+
+void max_heapify(int *data, int loc, int count) {
+    int left, right, largest, temp;
+    left = 2*(loc) + 1;
+    right = left + 1;
+    largest = loc;
+    
+
+    if (left <= count && data[left] > data[largest]) {
+        largest = left;
+    } 
+    if (right <= count && data[right] > data[largest]) {
+        largest = right;
+    } 
+    
+    if(largest != loc) {
+        temp = data[loc];
+        data[loc] = data[largest];
+        data[largest] = temp;
+        max_heapify(data, largest, count);
+    }
+
+}
+
+void heap_push(struct heap *h, int value)
+{
+    int index, parent;
+ 
+    // Resize the heap if it is too small to hold all the data
+    if (h->count == h->size)
+    {
+        h->size += 1;
+        h->heaparr = realloc(h->heaparr, sizeof(int) * h->size);
+        if (!h->heaparr) exit(-1); // Exit if the memory allocation fails
+    }
+    
+    index = h->count++; // First insert at last of array
+
+    // Find out where to put the element and put it
+    for(;index; index = parent)
+    {
+        parent = (index - 1) / 2;
+        if (h->heaparr[parent] >= value) break;
+        h->heaparr[index] = h->heaparr[parent];
+    }
+    h->heaparr[index] = value;
+}
+
+void heap_display(struct heap *h) {
+    int i;
+    for(i=0; i<h->count; ++i) {
+        printf("|%d|", h->heaparr[i]);
+    }
+    printf("\n");
+}
+
+int heap_delete(struct heap *h)
+{
+    int removed;
+    int temp = h->heaparr[--h->count];
+    
+    
+    if ((h->count <= (h->size + 2)) && (h->size > 0))
+    {
+        h->size -= 1;
+        h->heaparr = realloc(h->heaparr, sizeof(int) * h->size);
+        if (!h->heaparr) exit(-1); // Exit if the memory allocation fails
+    }
+    removed = h->heaparr[0];
+    h->heaparr[0] = temp;
+    max_heapify(h->heaparr, 0, h->count);
+    return removed;
+}
+
+
+int emptyPQ(struct heap *pq) {
+    int i;
+    while(pq->count != 0) {
+        printf("<<%d", heap_delete(pq));
+    }
+}
+
 int index_prog_atual;
 int pid_atual;
-
 
 // Testa se todos programas ja terminaram
 int allProgramsFinished(int finished[],int n)
--- scheduler.h
+++ scheduler.h
@@ -9,5 +9,45 @@
 #include <sys/types.h>
 #define MAX_PROG 10
 #define TAM 100
+#define TRUE  1
+#define FALSE   0
+
+/* a link in the queue, holds the info and point to the next Node*/
+typedef struct {
+    int info;
+} DATA;
+
+typedef struct Node_t {
+    DATA data;
+    struct Node_t *prev;
+} NODE;
+
+/* the HEAD of the Queue, hold the amount of node's that are in the queue*/
+typedef struct Queue {
+    NODE *head;
+    NODE *tail;
+    int size;
+    int limit;
+} Queue;
+
+// Queue implementation for round robin
+Queue *ConstructQueue(int limit);
+void DestructQueue(Queue *queue);
+int Enqueue(Queue *pQueue, NODE *item);
+NODE *Dequeue(Queue *pQueue);
+int isEmpty(Queue* pQueue);
+
+struct heap {
+	int size;
+	int count;
+	int *heaparr;
+};
+
+void heap_init(struct heap *h);
+void max_heapify(int *data, int loc, int count);
+void heap_push(struct heap *h, int value);
+void heap_display(struct heap *h);
+int heap_delete(struct heap *h);
+int emptyPQ(struct heap *pq);
 
 void scheduler(char prog[][TAM], int priority[], int iniRT[], int durationRT[], int qtdProgs);
